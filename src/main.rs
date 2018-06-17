ed9cea6d9b33cfe2e786ca2cc580fb1b20d11c2d
Sandeep Datta <datta.sandeep@gmail.com>
2018-06-17 16:56:43 +0530

    1. using serde_json. 2. Passing the --no-deps flag to cargo metadata.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 #[macro_use] extern crate error_chain;
-extern crate json;
+extern crate serde_json;
 
+use serde_json::Value;
 use std::process::Command;
 
 error_chain!{
     foreign_links {
         Io(std::io::Error);
         Utf8(std::string::FromUtf8Error);
-        Json(json::Error);
+        Json(serde_json::Error);
     }
 }
 
@@ -18,6 +19,7 @@ fn main() -> Result<()> {
     let output = Command::new(cargo_path)
                     .arg("metadata")
                     .arg("--format-version").arg("1")
+                    .arg("--no-deps")
                     .output()?;
 
     if !output.status.success() {
@@ -26,9 +28,9 @@ fn main() -> Result<()> {
 
     let meta_data = String::from_utf8(output.stdout)?;
 
-    let meta_data = json::parse(&meta_data)?;
+    let meta_data: Value = serde_json::from_str(&meta_data)?;
 
-    println!("{}", json::stringify_pretty(meta_data , 4));
+    println!("{}", serde_json::to_string_pretty(&meta_data)?);
 
     Ok(())
 }
